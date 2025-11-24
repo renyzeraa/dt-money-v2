f12c99152f107718331fd15b11edcd466a0ea958
Renan Silva <renansilvaytB@gmail.com>
2025-11-24 18:57:28 -0300

    feat: dialog and tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,35 +1,78 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        white: '#fff',
-
-        'gray-100': '#E1E1E6',
-        'gray-300': '#C4C4CC',
-        'gray-400': '#8D8D99',
-        'gray-500': '#7C7C8A',
-        'gray-600': '#323238',
-        'gray-700': '#29292E',
-        'gray-800': '#202024',
-        'gray-900': '#121214',
-
-        'green-300': '#00B37E',
-        'green-500': '#00875F',
-        'green-700': '#015F43',
-
-        'red-300': '#F75A68',
-        'red-500': '#AB222E',
-        'red-700': '#7A1921',
-      },
-      maxWidth: {
-        'container': '1120px',
-      }
-    },
+  	extend: {
+  		colors: {
+  			white: '#fff',
+  			'gray-100': '#E1E1E6',
+  			'gray-300': '#C4C4CC',
+  			'gray-400': '#8D8D99',
+  			'gray-500': '#7C7C8A',
+  			'gray-600': '#323238',
+  			'gray-700': '#29292E',
+  			'gray-800': '#202024',
+  			'gray-900': '#121214',
+  			'green-300': '#00B37E',
+  			'green-500': '#00875F',
+  			'green-700': '#015F43',
+  			'red-300': '#F75A68',
+  			'red-500': '#AB222E',
+  			'red-700': '#7A1921',
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		maxWidth: {
+  			container: '1120px'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 }
