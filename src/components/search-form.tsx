5fdc697a9f07898a0b2cf63d1bf486d3b2b35907
Renan Silva <renansilvaytB@gmail.com>
2025-11-25 17:44:38 -0300

    feat: validacao de formulario

--- src/components/search-form.tsx
+++ src/components/search-form.tsx
@@ -1,12 +1,41 @@
-import { MagnifyingGlass } from "phosphor-react";
+import { MagnifyingGlass } from "phosphor-react"
+import * as z from 'zod'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+const searchFormSchema = z.object({
+    query: z.string(),
+});
+
+type SearchFormInputs = z.infer<typeof searchFormSchema>;
 
 export function SearchForm() {
+    const {
+        register,
+        handleSubmit,
+        formState: { isSubmitting },
+    } = useForm<SearchFormInputs>({
+        resolver: zodResolver(searchFormSchema),
+    });
+
+    async function handleSearchTransactions(data: SearchFormInputs) {
+        await new Promise(resolve => setTimeout(resolve, 2000));
+
+        console.log(data);
+    }
+
     return (
-        <form className="flex gap-4">
-            <input type="text" placeholder="Busque por transações" className="flex-1 rounded-md border-none bg-gray-900 text-gray-300 p-4 placeholder:text-gray-500" />
+        <form className="flex gap-4" onSubmit={handleSubmit(handleSearchTransactions)}>
+            <input
+                type="text"
+                placeholder="Busque por transações"
+                className="flex-1 rounded-md border-none bg-gray-900 text-gray-300 p-4 placeholder:text-gray-500"
+                {...register('query')}
+            />
             <button
                 type="submit"
-                className="flex items-center gap-3 p-4 bg-transparent border border-solid border-green-300 text-green-300 font-bold rounded-md hover:bg-green-500 hover:border-green-500 hover:text-white hover:transition-colors"
+                className="flex items-center cursor-pointer disabled:cursor-not-allowed disabled:opacity-60 disabled:hover:bg-transparent disabled:hover:border-green-300 disabled:hover:text-green-300 gap-3 p-4 bg-transparent border border-solid border-green-300 text-green-300 font-bold rounded-md hover:bg-green-500 hover:border-green-500 hover:text-white hover:transition-colors"
+                disabled={isSubmitting}
             >
                 <MagnifyingGlass size={20} />
                 Buscar
--- src/components/transaction-modal.tsx
+++ src/components/transaction-modal.tsx
@@ -1,8 +1,33 @@
 import { ArrowCircleDown, ArrowCircleUp } from 'phosphor-react'
 import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from './ui/dialog'
 import { ToggleGroup, ToggleGroupItem } from './ui/toggle-group'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as z from 'zod'
+
+const newTransactionFormSchema = z.object({
+    description: z.string(),
+    price: z.number(),
+    category: z.string(),
+    // type: z.enum(['income', 'outcome']),
+})
+
+type NewTransactionFormInputs = z.infer<typeof newTransactionFormSchema>
 
 export function TransactionModal() {
+    const {
+        register,
+        handleSubmit,
+        formState: { isSubmitting }
+    } = useForm<NewTransactionFormInputs>({
+        resolver: zodResolver(newTransactionFormSchema),
+    })
+
+    async function handleCreateNewTransaction(data: NewTransactionFormInputs) {
+        await new Promise(resolve => setTimeout(resolve, 2000));
+        console.log(data)
+    }
+
     return (
         <Dialog>
             <DialogTrigger asChild>
@@ -18,10 +43,28 @@ export function TransactionModal() {
                     <DialogTitle>Nova transação</DialogTitle>
                 </DialogHeader>
 
-                <form action="" className='mt-8 flex flex-col gap-4'>
-                    <input type="text" className='rounded-md border-none bg-gray-900 text-gray-300 p-4 placeholder:text-gray-500' placeholder='Descrição' required />
-                    <input type="number" className='rounded-md border-none bg-gray-900 text-gray-300 p-4 placeholder:text-gray-500' placeholder='Preço' required />
-                    <input type="text" className='rounded-md border-none bg-gray-900 text-gray-300 p-4 placeholder:text-gray-500' placeholder='Categoria' required />
+                <form className='mt-8 flex flex-col gap-4' onSubmit={handleSubmit(handleCreateNewTransaction)}>
+                    <input
+                        type="text"
+                        className='rounded-md border-none bg-gray-900 text-gray-300 p-4 placeholder:text-gray-500'
+                        placeholder='Descrição'
+                        required
+                        {...register('description')}
+                    />
+                    <input
+                        type="number"
+                        className='rounded-md border-none bg-gray-900 text-gray-300 p-4 placeholder:text-gray-500'
+                        placeholder='Preço'
+                        required
+                        {...register('price', { valueAsNumber: true })}
+                    />
+                    <input
+                        type="text"
+                        className='rounded-md border-none bg-gray-900 text-gray-300 p-4 placeholder:text-gray-500'
+                        placeholder='Categoria'
+                        required
+                        {...register('category')}
+                    />
 
                     <ToggleGroup type="single" className='gap-4'>
                         <ToggleGroupItem value="income" asChild className='bg-gray-700 group hover:text-gray-100 hover:bg-gray-600 data-[state="on"]:bg-green-500 data-[state="on"]:text-gray-100'>
@@ -36,7 +79,7 @@ export function TransactionModal() {
                         </ToggleGroupItem>
                     </ToggleGroup>
 
-                    <button className='h-[58px] border-none bg-green-500 text-white font-bold px-5 hover:bg-green-700 transition-colors cursor-pointer rounded-md mt-6' type='submit'>Cadastrar</button>
+                    <button disabled={isSubmitting} className='disabled:opacity-60 disabled:cursor-not-allowed disabled:hover:bg-green-500 h-[58px] border-none bg-green-500 text-white font-bold px-5 hover:bg-green-700 transition-colors cursor-pointer rounded-md mt-6' type='submit'>Cadastrar</button>
                 </form>
             </DialogContent>
         </Dialog>
